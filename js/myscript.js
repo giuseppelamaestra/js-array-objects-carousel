5a5534fc1a89114c4e11598133bb7d53cdfbfcf1
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-05 20:01:22 +0200

    added css carousel structure

--- js/myscript.js
+++ js/myscript.js
@@ -21,3 +21,4 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
